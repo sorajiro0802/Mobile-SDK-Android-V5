40957ce9b54caf5317bf74a84104ef82befaf711
sorajiro0802 <sora_potato_0802@icloud.com>
2023-09-05 14:16:57 +0900

    add debug tag and connecting status check

--- SampleCode-V5/android-sdk-v5-sample/module-common/src/main/java/dji/sampleV5/modulecommon/models/LeicaControllerVM.kt
+++ SampleCode-V5/android-sdk-v5-sample/module-common/src/main/java/dji/sampleV5/modulecommon/models/LeicaControllerVM.kt
@@ -4,11 +4,13 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.os.Handler
+import android.util.Log
 import java.io.OutputStream
 import java.net.Socket
 import java.util.UUID
 
 class LeicaControllerVM() {
+    private val TAG = "LeicaControllerVM"
     var isConnected = false
     private val macAddr: String = "D4:36:39:77:DC:92" // MacAddress of Total Station Leica TS16
 
@@ -31,15 +33,21 @@ class LeicaControllerVM() {
         this.connectThread!!.start()
         try {
             Thread.sleep(2000)
+            this.isConnected = true
             return 0
         }catch (e: InterruptedException) {
             e.printStackTrace()
+            this.isConnected = false
             return -1
         }
     }
 
     fun read(){
-        mReceiveTask = BluetoothReceiveTask(this.connectThread!!.getSocket())
-        mReceiveTask!!.start()
+        if(this.isConnected) {
+            mReceiveTask = BluetoothReceiveTask(this.connectThread!!.getSocket())
+            mReceiveTask!!.start()
+        } else {
+            Log.e(TAG, "Leica is not connected")
+        }
     }
 }
