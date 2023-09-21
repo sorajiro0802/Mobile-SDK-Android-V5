22432dc818fac00c6a545596ede6571b77cc40ed
sorajiro0802 <sora_potato_0802@icloud.com>
2023-09-21 16:19:07 +0900

    Fix TCP/IP -> UDP/IP

--- SampleCode-V5/android-sdk-v5-sample/module-common/src/main/java/dji/sampleV5/modulecommon/models/TimeSyncVM.kt
+++ SampleCode-V5/android-sdk-v5-sample/module-common/src/main/java/dji/sampleV5/modulecommon/models/TimeSyncVM.kt
@@ -2,82 +2,72 @@ package dji.sampleV5.modulecommon.models
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 import java.net.InetSocketAddress
-import java.net.Socket
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 class TimeSyncVM() : DJIViewModel() {
-    var ip_addr : String = "192.168.0.98"
+    var ip : String = ""
     var port: Int = 8020
     val serverTime: MutableLiveData<String> = MutableLiveData()
-    var sc: SocketClient = SocketClient(ip_addr, port)
+    var sc: SocketClient = SocketClient(ip, port)
 
     fun sync(){
         sc.start()
     }
 
     // サーバに接続するクラス
-    inner class SocketClient(var ip:String, var port:Int): Thread(){
-        private lateinit var socket: Socket
-        private lateinit var reader: BufferedReader
+    inner class SocketClient(var ip: String, var port:Int): Thread(){
+        var inetIP: InetAddress = InetAddress.getByName(ip)
+        var stop_flag = false
+        var socket = DatagramSocket(port)
+
         private val receivedData: MutableLiveData<String> = this@TimeSyncVM.serverTime
 
         override fun run(){
-            this.connect()
             this.read()
         }
 
-        fun connect(){
-            try {
-                socket = Socket(ip, port)
-                Log.d(TAG, "connected socket")
-            } catch (e: Exception) {
-                Log.e(TAG, "$e")
-            }
-        }
-
         fun read(){
-            try {
-                !socket.isConnected
-            } catch (e:Exception) {
-                Log.e(TAG, "No Connection")
-                return
-            }
-            reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-            Log.d(TAG, "start reading")
-            try {
-                reader.use {
-                    while (true) {
-                        val message = it.readLine()
-                        if (message != null) {
-                            receivedData.postValue(message)
-                            Log.d(TAG, message)
-                        } else {
-                            break
-                        }
-                        sleep(SLEEP_TIME)
-                    }
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "$e")
+            while(!stop_flag){
+                send("send")
+                sleep(SLEEP_TIME)
+                recieve()
             }
         }
 
-        fun close() {
-            if (::reader.isInitialized) {
-                reader.close()
-                Log.d(TAG, "close reader")
-            }
-            if (::socket.isInitialized) {
-                socket.close()
-                Log.d(TAG, "close socket")
+        fun recieve(){
+//            val socket = DatagramSocket(port)
+            val buffer: ByteArray = ByteArray(24)
+            val packet: DatagramPacket = DatagramPacket(buffer, buffer.size)
+
+            try {
+                socket.receive(packet)
+                Log.d(TAG, String(buffer))
+            } catch (e: Exception) {
+                Log.e(TAG, "Could not receive data")
             }
         }
+
+        fun send(msg: String){
+//            val socket = DatagramSocket(port)
+            val byte = msg.toByteArray()
+            try {
+                val packet = DatagramPacket(byte, byte.size, inetIP, port)
+                socket.send(packet)
+            } catch(e: Exception) {
+                Log.e(TAG, "Could not send data")
+            }
+        }
+
+        fun close(){
+            socket.close()
+        }
+
     }
 
     fun getNowDate(): String {
@@ -88,19 +78,20 @@ class TimeSyncVM() : DJIViewModel() {
 
     fun setAddress(ip: String, port: Int){
         Log.d(TAG, "settled new addr ($ip,$port)")
-        this.ip_addr = ip
-        this.port = port
-        sc.ip = this.ip_addr
-        sc.port = this.port
+        this.ip = ip ; sc.ip = this.ip
+        this.port = port; sc.port = this.port
+        try { sc.inetIP = InetAddress.getByName(ip) }
+        catch(e: Exception) {e.printStackTrace()}
     }
 
     fun stop(){
-        this.sc.close()
+        sc.stop_flag = true
+        sc.close()
     }
 
 
     companion object {
         const val TAG = "TimeSyncVM"
-        const val SLEEP_TIME = 50L
+        const val SLEEP_TIME = 1000L
     }
 }
